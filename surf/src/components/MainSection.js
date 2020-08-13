eea73ba9515a7d70855b0bb9f139eb04c871f10a
paolosecci <paolosecci98@gmail.com>
2020-08-13 19:29:23 +0200

    ggsMain: create main section of gucci surf react app

---
+++ surf/src/components/MainSection.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+function MainSection() {
+    return (
+        <div className="main-section">
+            This is the main section!
+        </div>
+    );
+}
+
+export default MainSection;
